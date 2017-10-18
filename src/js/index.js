8263c1ebcd1c6c80aa0bf2507cd4459cb14bc5fa
yangjing <yangjing@kpkj.org>
2017-10-18 16:29:36 +0800

    5

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,13 @@
-(function() {
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        autoplay : 5000
-    });
+// (function() {
+//     // var swiper = new Swiper('.swiper-container', {
+//     //     pagination: '.swiper-pagination',
+//     //     paginationClickable: true,
+//     //     autoplay : 5000
+//     // });
+//
+// })();
 
-})();
+$(function() {
+    $('.slogin').stop().animate({top:'35%'},2000);
+    $('.content-wrap').stop().animate({bottom:'20px'},2000);
+});
