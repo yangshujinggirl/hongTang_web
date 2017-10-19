cd4cc367f47b17b2978361b6f5847e1b6078d7ba
yangjing <yangjing@kpkj.org>
2017-10-19 11:41:11 +0800

    4

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,11 @@
 // })();
 
 $(function() {
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        autoplay : 5000
+    });
     $('.slogin').stop().animate({top:'35%'},2000);
     $('.content-wrap').stop().animate({bottom:'20px'},2000);
 });
